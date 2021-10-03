306645a773a056db3f24f21362e9933bbc642e4c
Afreul kaium Joy <afreuljoy@gmail.com>
2021-10-04 00:47:32 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router ,Switch,Route} from 'react-router-dom';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import Service from './Components/Services/Service';
+import Blog from './Components/Blog/Blog';
+import About from './Components/About/About';
+import Error from './Components/Error/Error';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header></Header>
+      <Switch>
+
+        <Route path="/home">
+        <Home></Home>
+        </Route>
+
+        <Route path="/service">
+        <Service></Service>
+        </Route>
+
+        <Route path="/blog">
+        <Blog></Blog>
+        </Route>
+
+        <Route path="/about">
+        <About></About>
+        </Route>
+
+          <Route exact path="/">
+              <Home></Home>
+          </Route>
+
+          <Route path="*">
+             <Error></Error>
+          </Route>
+
+        </Switch>
+
+      <Footer></Footer>
+    </Router>
   );
 }
 
---
+++ src/Components/Blog/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blog = () => {
+  return (
+    <div>
+      <h3>Hello blog</h3>
+    </div>
+  );
+};
+
+export default Blog;
